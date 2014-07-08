8e03625af7df6f8529f0c814ac3eddd1d00e8f7c
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-07-08 23:20:43 +0200

    predisposizione cartelle per internazionalizzazione #32

---
+++ protected/messages/en/string.php
@@ -0,0 +1,25 @@
+<?php
+
+return array (
+    //MODELS
+    //GENERAL
+    'model.id' => 'ID',
+    'model.active' => 'Active',
+    'model.description' => 'Description',
+    'model.fromuser' => 'From User',
+    'model.lovecounter' => 'Lovecounter',
+    'model.thumbnail' => 'Thumbnail',
+    'model.title' => 'Title',
+    'model.createdat' => 'Created At',
+    'model.updatedat' => 'Update At',
+    
+    //VIDEO
+    'model.video.author' => 'Author',
+    'model.video.cover' => 'Cover',
+    'model.video.duration' => 'Duration',
+    'model.video.url' => 'URL',
+    
+    
+
+);
+?>
