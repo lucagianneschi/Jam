c5126a83e792203f5d7d400d1d4ec10c576deec1
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-07-14 21:11:13 +0200

    Prima generazione codice #37

---
+++ protected/controllers/CommentController.php
@@ -0,0 +1,315 @@
+<?php
+
+class CommentController extends Controller {
+
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+	return array(
+	    'accessControl', // perform access control for CRUD operations
+	    'postOnly + delete', // we only allow deletion via POST request
+	);
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+	return array(
+	    array('allow', // allow all users to perform 'index' and 'view' actions
+		'actions' => array('index', 'view'),
+		'users' => array('*'),
+	    ),
+	    array('allow', // allow authenticated user to perform 'create' and 'update' actions
+		'actions' => array('create', 'update'),
+		'users' => array('@'),
+	    ),
+	    array('allow', // allow admin user to perform 'admin' and 'delete' actions
+		'actions' => array('admin', 'delete'),
+		'users' => array('admin'),
+	    ),
+	    array('deny', // deny all users
+		'users' => array('*'),
+	    ),
+	);
+    }
+
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+	$this->render('view', array(
+	    'model' => $this->loadModel($id),
+	));
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate($id_object, $classType) {
+	switch ($classType) {
+	    case 'Album':
+		$object = Album::model()->findByPk($id_object);
+		break;
+	    case 'Comment':
+		$object = Comment::model()->findByPk($id_object);
+		break;
+	    case 'Event':
+		$object = Event::model()->findByPk($id_object);
+		break;
+	    case 'Image':
+		$object = Image::model()->findByPk($id_object);
+		break;
+	    case 'Record':
+		$object = Record::model()->findByPk($id_object);
+		break;
+	    case 'ReviewEvent':
+		$object = ReviewEvent::model()->findByPk($id_object);
+		break;
+	    case 'ReviewRecord':
+		$object = ReviewRecord::model()->findByPk($id_object);
+		break;
+	    case 'Video':
+		$object = Video::model()->findByPk($id_object);
+		break;
+	}
+	$touser = User::model()->findByPk($object->fromuser);
+	if ($touser === null)
+	    throw new CHttpException(404, 'The requested page does not exist.');
+
+	$fromuser = User::model()->findByPk(Yii::app()->session['id']);
+
+	if ($fromuser === null)
+	    throw new CHttpException(404, 'The requested page does not exist.');
+
+	$comment = new Comment;
+
+	// Uncomment the following line if AJAX validation is needed
+	$this->performAjaxValidation($comment);
+
+	if (isset($_POST['Comment'])) {
+	    $_POST['Post']['fromuser'] = $fromuser->id;
+	    $_POST['Post']['touser'] = $touser->id;
+	    $_POST['Post']['latitude'] = null;
+	    $_POST['Post']['longitude'] = null;
+	    $_POST['Post']['createdat'] = date('Y-m-d H:i:s');
+	    $_POST['Post']['updatedat'] = date('Y-m-d H:i:s');
+	    switch ($classType) {
+		case 'Album':
+		    $_POST['Post']['album'] = $id_object;
+		    break;
+		case 'Comment':
+		    $_POST['Post']['comment'] = $id_object;
+		    break;
+		case 'Event':
+		    $_POST['Post']['event'] = $id_object;
+		    break;
+		case 'Image':
+		    $_POST['Post']['image'] = $id_object;
+		    break;
+		case 'Record':
+		    $_POST['Post']['record'] = $id_object;
+		    break;
+		case 'ReviewEvent':
+		    $_POST['Post']['reviewevent'] = $id_object;
+		    break;
+		case 'ReviewRecord':
+		    $_POST['Post']['reviewrecord'] = $id_object;
+		    break;
+		case 'Video':
+		    $_POST['Post']['video'] = $id_object;
+		    break;
+	    }
+	    $comment->attributes = $_POST['Comment'];
+	    if ($comment->save())
+		$this->redirect(array('view', 'id' => $comment->id));
+	}
+	switch ($classType) {
+	    case 'Album':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'album' => $object
+		));
+		break;
+	    case 'Comment':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'comment' => $object
+		));
+		break;
+	    case 'Event':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'event' => $object
+		));
+		break;
+	    case 'Image':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'image' => $object
+		));
+		break;
+	    case 'Record':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'record' => $object
+		));
+		break;
+	    case 'ReviewEvent':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'reviewevent' => $object
+		));
+		break;
+	    case 'ReviewRecord':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'reviewrecord' => $object
+		));
+		break;
+	    case 'Video':
+		$this->render('create', array(
+		    'model' => $comment,
+		    'fromuser' => $fromuser,
+		    'touser' => $touser,
+		    'video' => $object
+		));
+		break;
+	}
+// manca fare il render dell'oggetto commentato
+	$this->render('create', array(
+	    'model' => $comment,
+	    'fromuser' => $fromuser,
+	    'touser' => $touser,
+	));
+    }
+
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+	$model = $this->loadModel($id);
+
+	// Uncomment the following line if AJAX validation is needed
+	// $this->performAjaxValidation($model);
+
+	if (isset($_POST['Comment'])) {
+	    $model->attributes = $_POST['Comment'];
+	    if ($model->save())
+		$this->redirect(array('view', 'id' => $model->id));
+	}
+
+	$this->render('update', array(
+	    'model' => $model,
+	));
+    }
+
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+	$this->loadModel($id)->delete();
+
+	// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+	if (!isset($_GET['ajax']))
+	    $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+	$dataProvider = new CActiveDataProvider('Comment');
+	$this->render('index', array(
+	    'dataProvider' => $dataProvider,
+	));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+	$model = new Comment('search');
+	$model->unsetAttributes();  // clear any default values
+	if (isset($_GET['Comment']))
+	    $model->attributes = $_GET['Comment'];
+
+	$this->render('admin', array(
+	    'model' => $model,
+	));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return Comment the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id) {
+	$model = Comment::model()->findByPk($id);
+	if ($model === null)
+	    throw new CHttpException(404, 'The requested page does not exist.');
+	return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param Comment $model the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+	if (isset($_POST['ajax']) && $_POST['ajax'] === 'comment-form') {
+	    echo CActiveForm::validate($model);
+	    Yii::app()->end();
+	}
+    }
+
+    /**
+     * Before action
+     * @param Aciton to be performed before save operation
+     */
+    public function beforeAction($action) {
+	if (parent::beforeAction($action)) {
+	    /* @var $cs CClientScript */
+	    $cs = Yii::app()->clientScript;
+
+	    $baseUrl = Yii::app()->baseUrl;
+
+	    $cs->registerCssFile($baseUrl . '/css/formWhiteStyle.css');
+
+
+	    return true;
+	}
+	return false;
+    }
+
+}
---
+++ protected/views/comment/_view.php
@@ -0,0 +1,87 @@
+<?php
+/* @var $this CommentController */
+/* @var $data Comment */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('active')); ?>:</b>
+	<?php echo CHtml::encode($data->active); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('album')); ?>:</b>
+	<?php echo CHtml::encode($data->album); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('comment')); ?>:</b>
+	<?php echo CHtml::encode($data->comment); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('commentcounter')); ?>:</b>
+	<?php echo CHtml::encode($data->commentcounter); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('event')); ?>:</b>
+	<?php echo CHtml::encode($data->event); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('fromuser')); ?>:</b>
+	<?php echo CHtml::encode($data->fromuser); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
+	<?php echo CHtml::encode($data->image); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('latitude')); ?>:</b>
+	<?php echo CHtml::encode($data->latitude); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('longitude')); ?>:</b>
+	<?php echo CHtml::encode($data->longitude); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('lovecounter')); ?>:</b>
+	<?php echo CHtml::encode($data->lovecounter); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('record')); ?>:</b>
+	<?php echo CHtml::encode($data->record); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('sharecounter')); ?>:</b>
+	<?php echo CHtml::encode($data->sharecounter); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('song')); ?>:</b>
+	<?php echo CHtml::encode($data->song); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('text')); ?>:</b>
+	<?php echo CHtml::encode($data->text); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('touser')); ?>:</b>
+	<?php echo CHtml::encode($data->touser); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('video')); ?>:</b>
+	<?php echo CHtml::encode($data->video); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('createdat')); ?>:</b>
+	<?php echo CHtml::encode($data->createdat); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updatedat')); ?>:</b>
+	<?php echo CHtml::encode($data->updatedat); ?>
+	<br />
+
+	*/ ?>
+
+</div>
