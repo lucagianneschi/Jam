3a8cadf1db9c262b047d845eabcbe93a160693d3
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-07-15 12:05:09 +0200

    funzione di recupero query valida per pagina uploadAlbum e profile #38

--- protected/models/Album.php
+++ protected/models/Album.php
@@ -27,129 +27,189 @@
  * @property Image[] $images
  * @property UserAlbum[] $userAlbums
  */
-class Album extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'album';
+class Album extends CActiveRecord {
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+	return 'album';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+	// NOTE: you should only define rules for those attributes that
+	// will receive user inputs.
+	return array(
+	    array('id, active, fromuser, title, createdat, updatedat', 'required', 'message' => '{attribute} field is missing'),
+	    array('active', 'boolean', 'message' => 'Invalid {attribute} format'),
+	    array('commentcounter, imagecounter, lovecounter, sharecounter', 'numerical', 'integerOnly' => true, 'message' => 'Invalid {attribute} format'),
+	    array('latitude, longitude', 'numerical', 'message' => 'Invalid {attribute} format'),
+	    array('cover, thumbnail', 'length', 'max' => 100, 'tooLong' => '{attribute} must be at most 100 characters'),
+	    array('fromuser', 'length', 'max' => 11, 'message' => 'Invalid {attribute} format'),
+	    array('title', 'length', 'max' => 80, 'tooLong' => '{attribute} must be at most 80 characters'),
+	    array('title', 'length', 'min' => 2, 'tooShort' => '{attribute} must be at least 2 characters'),
+	    array('description', 'length', 'max' => 3000, 'tooLong' => '{attribute} must be at most 3000 characters'),
+	    array('description', 'length', 'min' => 2, 'tooShort' => '{attribute} must be at least 2 characters'),
+	    array('createdat, updatedat', 'date', 'format' => 'Y-m-d H:m:s'),
+	    array('commentcounter, imagecounter, lovecounter, sharecounter', 'default', 'value' => 0),
+	    array('active', 'default', 'value' => 1),
+	    array('title, description', 'match', 'pattern' => '/^([a-zA-Z\xE0\xE8\xE9\xF9\xF2\xEC\x27]\s?)+$/', 'message' => 'Invalid {attribute}. No special characters allowed'),
+	    // The following rule is used by search().
+	    // @todo Please remove those attributes that should not be searched.
+	    array('id, active, commentcounter, description, fromuser, imagecounter, latitude, longitude, lovecounter, sharecounter, title, createdat, updatedat', 'safe', 'on' => 'search'),
+	);
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+	// NOTE: you may need to adjust the relation name and the related
+	// class name for the relations automatically generated below.
+	return array(
+	    'fromuser0' => array(self::BELONGS_TO, 'User', 'fromuser'),
+	    'albumTag' => array(self::HAS_ONE, 'AlbumTag', 'id_album'),
+	    'comments' => array(self::HAS_MANY, 'Comment', 'album'),
+	    'images' => array(self::HAS_MANY, 'Image', 'album'),
+	    'userAlbums' => array(self::HAS_MANY, 'UserAlbum', 'id_album'),
+	);
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+	return array(
+	    'id' => Yii::t('string', 'model.id'),
+	    'active' => Yii::t('string', 'model.active'),
+	    'commentcounter' => Yii::t('string', 'model.commentcounter'),
+	    'cover' => Yii::t('string', 'model.cover'),
+	    'description' => Yii::t('string', 'model.description'),
+	    'fromuser' => Yii::t('string', 'model.fromuser'),
+	    'imagecounter' => Yii::t('string', 'model.album.imagecounter'),
+	    'latitude' => Yii::t('string', 'model.latitude'),
+	    'longitude' => Yii::t('string', 'model.longitude'),
+	    'lovecounter' => Yii::t('string', 'model.lovecounter'),
+	    'sharecounter' => Yii::t('string', 'model.sharecounter'),
+	    'thumbnail' => Yii::t('string', 'model.thumbnail'),
+	    'title' => Yii::t('string', 'model.title'),
+	    'createdat' => Yii::t('string', 'model.createdat'),
+	    'updatedat' => Yii::t('string', 'model.updatedat'),
+	);
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+	// @todo Please modify the following code to remove attributes that should not be searched.
+
+	$criteria = new CDbCriteria;
+
+	$criteria->compare('id', $this->id, true);
+	$criteria->compare('active', $this->active);
+	$criteria->compare('commentcounter', $this->commentcounter);
+	$criteria->compare('description', $this->description, true);
+	$criteria->compare('fromuser', $this->fromuser, true);
+	$criteria->compare('imagecounter', $this->imagecounter);
+	$criteria->compare('latitude', $this->latitude);
+	$criteria->compare('longitude', $this->longitude);
+	$criteria->compare('lovecounter', $this->lovecounter);
+	$criteria->compare('sharecounter', $this->sharecounter);
+	$criteria->compare('title', $this->title, true);
+	$criteria->compare('createdat', $this->createdat, true);
+	$criteria->compare('updatedat', $this->updatedat, true);
+
+	return new CActiveDataProvider($this, array(
+	    'criteria' => $criteria,
+	));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Album the static model class
+     */
+    public static function model($className = __CLASS__) {
+	return parent::model($className);
+    }
+
+    /**
+     * Returns an array of album for the the profile page
+     * @param integer $id id of the user who own the page
+     * @param integer $limit number of album to be displayed
+     * @param integer $skip number of album to be skipped
+     * @return array $albums array of onfo to be displayed on the profile page, false in case of error
+     */
+    public function profileOrUpload($id, $limit = 3, $skip = 0) {
+	$dbConnection = new DBConnection();
+	$connection = $dbConnection->connect();
+	if ($connection === false) {
+	    return false;
 	}
-
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('id, active, fromuser, title, createdat, updatedat', 'required','message'=>'{attribute} field is missing'),
-		        array('active', 'boolean', 'message'=>'Invalid {attribute} format'),
-			array('commentcounter, imagecounter, lovecounter, sharecounter', 'numerical', 'integerOnly'=>true,'message'=>'Invalid {attribute} format'),
-			array('latitude, longitude', 'numerical','message'=>'Invalid {attribute} format'),
-			array('cover, thumbnail', 'length', 'max'=>100,'tooLong'=>'{attribute} must be at most 100 characters'),
-			array('fromuser', 'length', 'max'=>11,'message'=>'Invalid {attribute} format'),
-			array('title', 'length', 'max'=>80,'tooLong'=>'{attribute} must be at most 80 characters'),
-		        array('title', 'length', 'min'=>2,'tooShort'=>'{attribute} must be at least 2 characters'),
-		    	array('description', 'length', 'max'=>3000,'tooLong'=>'{attribute} must be at most 3000 characters'),
-		        array('description', 'length', 'min'=>2,'tooShort'=>'{attribute} must be at least 2 characters'),
-			array('createdat, updatedat', 'date', 'format' =>  'Y-m-d H:m:s'),
-		        array('commentcounter, imagecounter, lovecounter, sharecounter', 'default', 'value'=>0),
-		        array('active', 'default', 'value'=>1),
-		        array('title, description', 'match', 'pattern'=>'/^([a-zA-Z\xE0\xE8\xE9\xF9\xF2\xEC\x27]\s?)+$/', 'message' => 'Invalid {attribute}. No special characters allowed'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, active, commentcounter, description, fromuser, imagecounter, latitude, longitude, lovecounter, sharecounter, title, createdat, updatedat', 'safe', 'on'=>'search'),
-		);
+	$albums = array();
+	$sql = "SELECT a.id id_a,
+                   a.commentcounter,
+                   a.fromuser,
+                   a.imagecounter,
+                   a.lovecounter,
+                   a.sharecounter,
+                   a.thumbnail thumbnail_a,
+                   a.title,
+                   a.createdat,
+                   u.id id_u,
+                   u.username,
+                   u.thumbnail thumbnail_u,
+                   u.type
+              FROM album a,
+                   user u
+             WHERE a.active = 1
+               AND a.fromuser =" . $id .
+		"ORDER BY a.createdat ASC
+             LIMIT" . $limit .
+		"SKIP" . $skip;
+	$results = mysqli_query($connection, $sql);
+	if (!$results) {
+	    return false;
 	}
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'fromuser0' => array(self::BELONGS_TO, 'User', 'fromuser'),
-			'albumTag' => array(self::HAS_ONE, 'AlbumTag', 'id_album'),
-			'comments' => array(self::HAS_MANY, 'Comment', 'album'),
-			'images' => array(self::HAS_MANY, 'Image', 'album'),
-			'userAlbums' => array(self::HAS_MANY, 'UserAlbum', 'id_album'),
-		);
+	while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+	    $rows_album[] = $row;
+	if (!is_array($rows_album)) {
+	    return $albums;
 	}
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-		        'id'=>Yii::t('string','model.id'),
-		        'active'=>Yii::t('string','model.active'),
-		        'commentcounter'=>Yii::t('string','model.commentcounter'),
-			'cover'=>Yii::t('string','model.cover'),
-			'description'=>Yii::t('string','model.description'),
-			'fromuser'=>Yii::t('string','model.fromuser'),
-		        'imagecounter'=>Yii::t('string','model.album.imagecounter'),
-			'latitude'=>Yii::t('string','model.latitude'),
-		        'longitude'=>Yii::t('string','model.longitude'),
-			'lovecounter'=>Yii::t('string','model.lovecounter'),
-		        'sharecounter'=>Yii::t('string','model.sharecounter'),
-			'thumbnail'=>Yii::t('string','model.thumbnail'),
-			'title'=>Yii::t('string','model.title'),
-			'createdat'=>Yii::t('string','model.createdat'),
-		        'updatedat'=>Yii::t('string','model.updatedat'),
-		);
+	foreach ($rows_album as $row) {
+	    $album = new Album;
+	    $album->id = $row['id_a'];
+	    $album->commentcounter = $row['commentcounter'];
+	    $fromuser = new User;
+	    $fromuser->id = $row['id_u'];
+	    $fromuser->thumbnail = $row['thumbnail_u'];
+	    $fromuser->username = $row['username'];
+	    $fromuser->type = $row['type'];
+	    $album->fromuser = $fromuser;
+	    $album->imagecounter = $row['imagecounter'];
+	    $album->lovecounter = $row['lovecounter'];
+	    $album->sharecounter = $row['sharecounter'];
+	    $album->thumbnail = $row['thumbnail_a'];
+	    $album->title = $row['title'];
+	    $album->createdat = new DateTime($row['createdat']);
+	    $album->updatedat = new DateTime($row['updatedat']);
+	    $albums[$row['id_a']] = $album;
 	}
+	return $albums;
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('active',$this->active);
-		$criteria->compare('commentcounter',$this->commentcounter);
-		$criteria->compare('description',$this->description,true);
-		$criteria->compare('fromuser',$this->fromuser,true);
-		$criteria->compare('imagecounter',$this->imagecounter);
-		$criteria->compare('latitude',$this->latitude);
-		$criteria->compare('longitude',$this->longitude);
-		$criteria->compare('lovecounter',$this->lovecounter);
-		$criteria->compare('sharecounter',$this->sharecounter);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('createdat',$this->createdat,true);
-		$criteria->compare('updatedat',$this->updatedat,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Album the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 }
