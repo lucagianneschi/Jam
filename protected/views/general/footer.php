463263c81f2859afbc847efa001ff8ff3e45123d
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-07-29 16:22:37 +0200

    recupero file general/ #58

---
+++ protected/views/general/footer.php
@@ -0,0 +1,75 @@
+<?php
+$username = '';
+$id = '';
+if (isset($_SESSION['id'])) {
+    $id = $_SESSION['id'];
+    $username = $_SESSION['username'];
+}
+?>
+<footer id="footer" >
+    <div id="footer-header">
+	<div class="row">
+	    <div class="large-2 columns hide-for-small" onclick="footerShow()">
+		<div class="user grey"><?php echo Yii::t('string', 'views.footer.staytunedon'); ?></div>
+	    </div>
+	    <div class="large-5 small-6 columns" onclick="footerShow()">
+		<a class="ico-label _facebook"></a>
+		<a class="ico-label _twitter"></a>
+		<a class="ico-label _google"></a>
+		<a class="ico-label _youtube"></a>
+		<a class="ico-label _web"></a>				
+	    </div>
+	    <div class="large-5 small-1 columns align-right">
+		<div class="row">
+		    <div class="large-1 small-1 columns" style="float: right; padding: 0px">
+			<a class="ico-label _off" onclick="access(null, null, 'logout', null)"></a>
+		    </div>
+		    <div class="large-11 columns hide-for-small align-right" style="padding: 0px">						
+			<div class="user"><a class="user" onclick="location.href = 'profile.php?user=<?php echo $id ?>'"><?php echo $username; ?></a></div>									
+		    </div>		
+		</div>						
+	    </div>
+	</div>
+    </div>
+    <div id="footer-body" class="no-display">
+	<div class="row">
+	    <div class="large-12 columns">					
+		<br>	
+		<div class="row">
+		    <div class="small-3 columns">
+			<h5><?php echo $views['footer']['aboutjam']; ?></h5>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.aboutus'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.virtualtour'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.career'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.term_cond'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.licenses'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.pricing'); ?></a><br>
+		    </div>
+		    <div class="small-3 columns">
+			<h5><?php echo Yii::t('string', 'views.footer.support'); ?></h5>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.guide'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.faq'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.plugin'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.refpolicy'); ?></a><br>
+		    </div>	
+		    <div class="small-3 columns">
+			<h5><?php echo Yii::t('string', 'views.footer.resources'); ?></h5>							
+			<a href="#"><?php echo Yii::t('string', 'views.footer.api'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.mobile'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.logos_badge'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.pressres'); ?></a><br>
+			<a href="#"><?php echo Yii::t('string', 'views.footer.advres'); ?></a><br>
+		    </div>	
+		    <div class="small-3 columns">
+			<h5><?php echo Yii::t('string', 'views.footer.contact'); ?></h5>
+			<a href="#"><?php Yii::t('string', 'views.footer.abuse'); ?></a><br>
+			<a href="#"><?php Yii::t('string', 'views.footer.writeus'); ?></a><br>
+			<a href="#"><?php Yii::t('string', 'views.footer.newsletter'); ?></a>
+		    </div>		
+		</div>
+	    </div>
+	</div>
+    </div>	
+</footer>
+</body>
+</html>
---
+++ protected/views/general/meta.php
@@ -0,0 +1,62 @@
+<meta charset="utf-8" />
+
+<!-- Set the viewport width to device width for mobile -->
+<meta name="viewport" id="vp" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+<meta name="viewport" id="vp" content="initial-scale=1.0,user-scalable=no,maximum-scale=1" media="(device-height: 568px)" />
+<meta name="format-detection" content="telephone=no" />
+<!-- Mobile -->
+<link rel="apple-touch-icon" href="images/icon/touch-icon-iphone.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="images/icon/touch-icon-ipad.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="images/icon/touch-icon-iphone-retina.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="images/icon/touch-icon-ipad-retina.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+<!-- font style -->
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,400italic,600,700,200' rel='stylesheet' type='text/css' />
+
+<!-- fountation style -->
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/app.css" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/profilestyle.css" />
+<link rel="stylesheet" href="css/headerstyle.css" /> 
+<link rel="stylesheet" href="css/footerStyle.css" /> 
+<link rel="stylesheet" href="css/formBlackStyle.css" />
+<link rel="stylesheet" href="css/formWhiteStyle.css" />
+<link rel="stylesheet" href="css/signupStyle.css" />
+<link rel="stylesheet" href="css/uploadRecordStyle.css" />
+<link rel="stylesheet" href="css/uploadEventStyle.css" />
+<link rel="stylesheet" href="css/uploadAlbumStyle.css" />
+<link rel="stylesheet" href="css/messageStyle.css" />
+<!---------------- touchCarouser ------------------------>
+<link rel="stylesheet" href="css/touchCarousel/touchcarousel.css" />
+<link rel="stylesheet" href="css/touchCarousel/grey-blue-skin.css" />
+<!---------------- jcrop style--------------------------->
+<link rel="stylesheet" href="css/jquery.Jcrop.min.css" type="text/css"></link>
+<script src="js/plugins/vendor/custom.modernizr.js"></script>
+<!----------------- royalslider ------------------------->
+<link rel="stylesheet" href="css/royalslider/royalslider.css"></link>
+<link rel="stylesheet" href="css/royalslider/rs-default.css"></link>
+<link class="rs-file" href="css/royalslider/rs-minimal-white.css" rel="stylesheet"></link>
+<!----------------- colorbox ---------------------------->
+<link rel="stylesheet" href="css/colorbox/colorbox.css"></link>
+<!--------------- scrollbar ------------------------------->
+<link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.css"  />
+<!--------------- rating ---------------------------------->
+<link rel="stylesheet" href="css/rating/jquery.rating.css"  /> 
+
+<!---------------------------------- JQUERY  --------------------------------------
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>---->
+<script src="js/plugins/jquery/jquery-1.8.3.min.js"></script>
+<script src="js/plugins/jquery/jquery.easing.1.3.js"></script>	
+
+<!----------------------------------- PLAYLIST ---------------------------------->
+
+<link href="css/jplayer/jplayer.pink.flag.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/plugins/jplayer/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="js/plugins/jplayer/jplayer.playlist.min.js"></script>
+<script type="text/javascript" src="js/customs/header.js"></script>
+
+<!----------------------------------- FAVICON ---------------------------------->
+<link rel="shortcut icon" href="images/icon/favicon.ico"/>
