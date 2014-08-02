913c3cca1bdd171b11dbaee58fa6f18db81cf1bb
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-08-02 16:54:52 +0200

    aggiunti home, profile e test

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,47 +1,52 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
+    <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl?>/css/main.css">	
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl ?>/css/main.css">	
+	    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
 
-<body>
+    <body>
 
-<div class="container" id="page">
+	<div class="container" id="page">
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-			/*	array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),*/
-				array('label'=>'Gii', 'url'=>array('/gii')),
-				array('label'=>'Review Event', 'url'=>array('/reviewEvent/create','event'=>1)), 
-			    array('label'=>'Review Record', 'url'=>array('/reviewRecord/create','record'=>1)),
-			    array('label'=>'Event', 'url'=>array('/event/create')),
-			    array('label'=>'Post', 'url'=>array('/post/create','id'=>1)),
-			    array('label'=>'Comment', 'url'=>array('/comment/create','id'=>1, 'class'=>'event')),
-			    array('label'=>'Message', 'url'=>array('/message/create','id'=>2)),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),				
-			),
-		)); ?>
-	</div><!-- mainmenu -->
+	    <div id="mainmenu">
+		<?php
+		$this->widget('zii.widgets.CMenu', array(
+		    'items' => array(
+			/* 	array('label'=>'Home', 'url'=>array('/site/index')),
+			  array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')), */
+			array('label' => 'Gii', 'url' => array('/gii')),
+			array('label' => 'Home', 'url' => array('/site/home'),'visible' => Yii::app()->user->isGuest),
+			array('label' => 'Profile', 'url' => array('/site/profile','id' => 1)),
+			array('label' => 'Test', 'url' => array('/site/test')),
+			array('label' => 'Review Event', 'url' => array('/reviewEvent/create', 'event' => 1)),
+			array('label' => 'Review Record', 'url' => array('/reviewRecord/create', 'record' => 1)),
+			array('label' => 'Event', 'url' => array('/event/create')),
+			array('label' => 'Post', 'url' => array('/post/create', 'id' => 1)),
+			array('label' => 'Comment', 'url' => array('/comment/create', 'id' => 1, 'class' => 'event')),
+			array('label' => 'Message', 'url' => array('/message/create', 'id' => 2)),
+			array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+			array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
+		    ),
+		));
+		?>
+	    </div><!-- mainmenu -->
 
-	<?php echo $content; ?>
+<?php echo $content; ?>
 
-	<div class="clear"></div>
+	    <div class="clear"></div>
 
-	<div id="footer">
-		
-	</div><!-- footer -->
-	
-	<script>
-	    $(document).foundation();
-	</script>
-</div><!-- page -->
+	    <div id="footer">
 
-</body>
+	    </div><!-- footer -->
+
+	    <script>
+		$(document).foundation();
+	    </script>
+	</div><!-- page -->
+
+    </body>
 </html>
