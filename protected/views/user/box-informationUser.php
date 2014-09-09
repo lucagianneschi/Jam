43daa8e2c3d66bf6d9ceec8ffb1b70a11617ef34
Luca Gianneschi <luca.gianneschi@gmail.com>
2014-09-09 15:24:03 +0200

    revision box relative a dati utente. Si use unico box #69

--- protected/views/user/box-informationUser.php
+++ protected/views/user/box-informationUser.php
@@ -8,50 +8,45 @@
  * 
  * box chiamato tramite load con:
  * data: array conente infomazoini di tipo userInfo, 
+ * 
+ * 
  */
-$id = $_POST['id'];
-$featurings = array();
 
-$featurings = getRelatedNodes('user', $id, 'event', 'featuring');
+$id = $_POST['id'];
+$connectionService = new ConnectionService();
+$featurings = getRelatedNodes($connectionService, 'user', $id, 'record', 'featuring');
+
 $featuringsCounter = count($featurings);
 
 if ($featuringsCounter > 0) {
     ?>
-    <p class="title" data-section-title><a href="#"><?php echo Yii::t('string', 'view.media.information.content2'); ?></a></p>
+    <p class="title" data-section-title><a href="#"><?php echo Yii::t('string', 'view.media.info.content2'); ?></a></p>
     <div class="content" data-section-content>
         <div class="row">
 	    <?php
-	    $totalView = $featuringsCounter > 6 ? 6 : $featuringsCounter;
+	    $totalView = $featuringsCounter > 4 ? 4 : $featuringsCounter;
 	    $i = 1;
 	    foreach ($featurings as $key => $value) {
-		switch ($value['type']) {
-		    case 'JAMMER':
-			$defaultThum = Yii::app()->params['defaultImages']['DEFTHUMBJAMMER'];
-			break;
-		    case 'VENUE':
-			$defaultThum = Yii::app()->params['defaultImages']['DEFTHUMBVENUE'];
-			break;
-		}
+		$defaultThum = $value['type'] == 'JAMMER' ? Yii::app()->params['defaultImages']['DEFTHUMBJAMMER'] : Yii::app()->params['defaultImages']['DEFTHUMBVENUE'];
+		$fileManagerService = new FileManagerService();
+		$pathPicture = $fileManagerService->getUserAvatarPath($value['id'], $value['thumbnail'], true);
 		?>
 		<div  class="small-6 columns">
-		    <div class="box-membre" onclick="location.href = 'profile.php?user=<?php echo $value['id']; ?>'">
-			<div class="row " id="featuring_<?php echo $value['id']; ?>">
-			    <div  class="small-3 columns ">
-				<div class="icon-header">
-				    <!-- THUMB USER-->
-				    <?php
-				    $fileManagerService = new FileManagerService();
-				    $thumbPath = $fileManagerService->getUserAvatarPath($value['id'], $value['thumbnail'], true);
-				    ?>
-				    <img src="<?php echo $thumbPath; ?>" onerror="this.src='<?php echo $defaultThum; ?>'" alt ="<?php echo $value['username']; ?> ">
+		    <a href="profile.php?user=<?php echo $value['id']; ?> ">
+			<div class="box-membre">
+			    <div class="row ">
+				<div  class="small-3 columns ">
+				    <div class="icon-header">
+					<img src="<?php echo $pathPicture . $value['thumbnail']; ?>" onerror="this.src='<?php echo $defaultThum; ?>'" alt ="<?php echo $value['username']; ?> ">
+				    </div>
 				</div>
+				<div  class="small-9 columns ">
+				    <div class="text white breakOffTest"><strong><?php echo $value['username']; ?></strong></div>
+				    <small class="orange"><?php echo $value['type']; ?></small>
+				</div>		
 			    </div>
-			    <div  class="small-9 columns ">
-				<div class="text white breakOffTest"><strong><?php echo $value['username']; ?></strong></div>
-				<small class="orange"><?php echo $value['type']; ?></small>
-			    </div>		
 			</div>
-		    </div>
+		    </a>
 		</div>
 		<?php
 		if ($i % 2 == 0) {
